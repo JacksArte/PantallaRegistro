e281dabb211a25b7abae8c1ad5cc0e23b3df288b
JacksArte <jaqui2000arte@gmail.com>
2022-06-09 23:11:25 -0500

    Segundo Commit

--- wear/src/main/java/com/example/pruebawear01/MainActivity.java
+++ wear/src/main/java/com/example/pruebawear01/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,28 +16,30 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.example.pruebawear01.databinding.ActivityMainBinding;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends Activity {
 
     private Button wBotton = null;
+
     private TextView mTextView;
     private ActivityMainBinding binding;
-
     private Intent intent;
     private PendingIntent pendingIntent;
-
-    private NotificationCompat.Builder notofication;
+    private NotificationCompat.Builder notification;
+    private NotificationCompat.Builder notification2;
     private NotificationManagerCompat nm;
     private NotificationCompat.WearableExtender wearableExtender;
+
 
     String idChannel = "Mi_Canal";
     int idNotification = 001;
 
     private NotificationCompat.BigTextStyle bigTextStyle;
 
-    String longText = "with BigStyle, only a single line of text would be visible" +
-            "Any additional text would not appear directly in the notification";
-
+    String longText = "Mi notificacion";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,34 +55,64 @@ public class MainActivity extends Activity {
         nm = NotificationManagerCompat.from(MainActivity.this);
 
         wearableExtender = new NotificationCompat.WearableExtender();
-        bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle = new NotificationCompat.BigTextStyle().bigText(longText);
 
         wBotton.setOnClickListener(new View.OnClickListener() {
+
+            public void start(View view){
+                Timer t = new Timer();
+                TimerTask tt = new TimerTask() {
+                    @Override
+                    public void run() {
+                        int importance = NotificationManager.IMPORTANCE_HIGH;
+                        String name = "Notification 2";
+                        NotificationChannel notificationChannel = new NotificationChannel(idChannel, name, importance);
+
+                        nm.createNotificationChannel(notificationChannel);
+
+                        pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
+
+                        notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("Mi notification")
+                                .setContentText("Mi segunda notificacion wear")
+                                .extend(wearableExtender);
+                        nm.notify(idNotification, notification.build());
+                    }
+                };
+
+                t.schedule(tt, 5000);
+
+            }
+
             @Override
             public void onClick(View view) {
+                start(view);
                 int importance = NotificationManager.IMPORTANCE_HIGH;
-                String name = "Notificacion";
-
-                NotificationChannel notificationChannel =
-                        new NotificationChannel(idChannel, name, importance);
+                String name = "Notification";
+                NotificationChannel notificationChannel = new NotificationChannel(idChannel, name, importance);
 
                 nm.createNotificationChannel(notificationChannel);
 
-                pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+                pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
 
-                notofication = new NotificationCompat.Builder(MainActivity.this, idChannel)
-
+                notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
                         .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("Notification Wear")
-                        .setContentText(longText)
-                        .setContentIntent(pendingIntent)
-                        .extend(wearableExtender)
-                        .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
-                        .setStyle(bigTextStyle);
-
-                nm.notify(idNotification, notofication.build());
+                        .setContentTitle("Mi notification")
+                        .setContentText("Mi primera notificacion wear")
+                        .extend(wearableExtender);
+                nm.notify(idNotification, notification.build());
             }
-
         });
     }
+  /*notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Notification Wear")
+                        .setContentIntent(pendingIntent)
+                        .extend(wearableExtender)
+                        .setStyle(bigTextStyle)
+                        .setVibrate(new long[]{100,200,300,400,500,400,500,400})
+                        .setStyle(bigTextStyle);*/
+
+
 }
